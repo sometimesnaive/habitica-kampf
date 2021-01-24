9862947dd3f2fc18412d1b2d1dedab5ebd4f6bc8
sometimesnaive <doggg2@gmail.com>
2021-01-24 11:37:48 -0600

    view hooks

--- functions/hooks.js
+++ functions/hooks.js
@@ -7,11 +7,30 @@ const HEADERS = {
 
 
 exports.handler = async (event, context) => {
+    if (event.queryStringParameters) {
+        if (event.queryStringParameters.enable == 'true') {
+            callApi(
+                'https://habitica.com/api/v3/user/webhook',
+                {
+                    "enabled": true,
+                    "url": "https://eloquent-wozniak-5a5cc0.netlify.app/.netlify/functions/hello",
+                    "label": "My Quest Webhook",
+                    "type": "questActivity",
+                    "options": { // set at least one to true
+                    "questStarted": true,  // default
+                    "questFinished": true, // default
+                    "questInvited": true,  // default
+                    }
+                },
+            ); 
+        }
+
+    }
     const hooks = await getWebhooks();
     // return so Habitica does not wait for too long
     return {
         statusCode: 200,
-        body: hooks,
+        body: JSON.stringify(hooks, null, 2) + JSON.stringify(event.queryStringParameters),
     };
 }
 
@@ -23,6 +42,18 @@ async function getWebhooks() {
     }
 
     const url = "https://habitica.com/api/v3/user/webhook";
+    const response = await fetch(url, params);
+    return response.json();
+}
+
+async function callApi(url, data, method='POST') {
+    const params = {
+        "method" : method,
+        "headers" : {...HEADERS, 'Content-Type': 'application/json'},
+        "body": JSON.stringify(data),
+        "muteHttpExceptions" : true,
+    };
+
     const response = await fetch(url, params);
     return response.json();
 }
